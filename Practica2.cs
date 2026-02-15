f012440d302e685b3bd2fae1eb9ad25b4adfc4c4
luis160401 <126734934+luis160401@users.noreply.github.com>
2026-02-15 02:28:29 -0400

    Add files via upload

---
+++ Practica2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace MyProgram
+{
+    class Practica2
+    {
+        static void Main(string[] args0)
+        {   
+            Console.WriteLine($"Bienvenido al determinador de pares o Impares/n");
+            Console.WriteLine($"-----------------------------------------------");
+            Console.Write($"Escribe el numero : ");
+            int num= int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0 )
+                {Console.WriteLine($"El numero {num} es par");}
+
+            else 
+                {Console.WriteLine($"El numero {num} es impar");}
+        }
+    }
+}
